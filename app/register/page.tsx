b8a204a5902624879d1338c2ffccd5708f2aa423
informatica10 <alexteotonio@bioextratus.rede.bioextratus>
2024-09-24 23:53:25 -0300

    Remover chave

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -43,7 +43,7 @@ export default function Register() {
 
     try {
       const res = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}}/usuarios/cadastro`,
+        `${process.env.NEXT_PUBLIC_API_URL}/usuarios/cadastro`,
         {
           method: "POST",
           headers: {
